2d588366de4e9e983f2f20045c280f760d413955
pavelklindziuk <pavel_klindziuk@epam.com>
2020-04-23 17:07:02 +0300

    EVBX-06: fix formatter

--- src/main/java/com/evbx/product/layer/controller/ProductController.java
+++ src/main/java/com/evbx/product/layer/controller/ProductController.java
@@ -5,7 +5,6 @@ import com.evbx.product.layer.service.ProductService;
 import com.evbx.product.model.domain.Product;
 import com.evbx.product.model.data.ItemData;
 import com.evbx.product.model.dto.ProductDto;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -35,33 +34,24 @@ public class ProductController {
     }
 
     @GetMapping(value = "/products/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<ProductDto> getProduct(
-            @PathVariable
-                    long id) {
+    public ResponseEntity<ProductDto> getProduct(@PathVariable long id) {
         return ResponseEntity.ok(productService.getProduct(id));
     }
 
     @PostMapping(value = "/products", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Product> addProduct(@Valid
-    @RequestBody
-            Product product) {
+    public ResponseEntity<Product> addProduct(@Valid @RequestBody Product product) {
         return ResponseEntity.ok(productService.save(product));
     }
 
     @PatchMapping(value = "/products/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Product> updateProduct(
-            @PathVariable
-                    long id,
-            @RequestBody
-                    Product product) {
+    public ResponseEntity<Product> updateProduct(@PathVariable long id, @RequestBody Product product) {
         return ResponseEntity.ok(productService.update(id, product));
     }
 
     @DeleteMapping(value = "/products/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<String> deleteProduct(
-            @PathVariable
-                    long id) {
+    public ResponseEntity<String> deleteProduct(@PathVariable long id) {
         productService.deleteById(id);
-        return ResponseEntity.ok(String.format("%s item with id = %d successfully deleted.", Item.PRODUCT, id));
+        return ResponseEntity.ok(
+                String.format("%s item with id = %d successfully deleted.", Item.PRODUCT, id));
     }
 }
